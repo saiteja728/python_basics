a9a17edc32a959cfc1d1cb3ef0cd270e8100506b
saiteja728 <133037014+saiteja728@users.noreply.github.com>
2023-11-11 10:47:47 +0530

    Add files via upload

---
+++ numpyIntro.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+result = arr1 + arr2
+
+print("Array Addition:")
+print(result)
+
+#multiplication
+arr3 = arr1 * arr2
+
+print("\nElement-wise Multiplication:")
+print(arr3)
+
+arr = np.array([1, 4, 9])
+sqrt_arr = np.sqrt(arr)
+
+print("Square Root:")
+print(sqrt_arr)
+
+# Trigonometric functions
+angles = np.array([0, np.pi/2, np.pi])
+sin_values = np.sin(angles)
+
+print("\nSin Values:")
+print(sin_values)
+
+# Creating a 1D array
+arr1 = np.array([1, 2, 3])
+
+# Creating a 2D array
+arr2 = np.array([[1, 2, 3], [4, 5, 6]])
+
+print("1D Array:")
+print(arr1)
+print("\n2D Array:")
+print(arr2)
+
+
+
+
+# Array addition
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+result = arr1 + arr2
+
+print("Array Addition:")
+print(result)
+
+# Element-wise multiplication
+arr3 = arr1 * arr2
+
+print("\nElement-wise Multiplication:")
+print(arr3)
+
+# Square root
+arr = np.array([1, 4, 9])
+sqrt_arr = np.sqrt(arr)
+
+print("Square Root:")
+print(sqrt_arr)
+
+# Trigonometric functions
+angles = np.array([0, np.pi/2, np.pi])
+sin_values = np.sin(angles)
+
+print("\nSin Values:")
+print(sin_values)
+
+
+arr = np.array([1, 2, 3, 4, 5])
+
+# Accessing elements
+print("Element at index 2:", arr[2])
+
+# Slicing
+print("Sliced array:", arr[1:4])
+
+
+
+
+
+
+
